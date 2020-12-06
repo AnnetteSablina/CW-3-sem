af99c60cca02cd0915aa544fd203a9f6fd271339
Annette <oiveshka@gmail.com>
2020-12-06 21:00:28 +0300

    Update User.cpp

--- CW 3/User.cpp
+++ CW 3/User.cpp
@@ -5,28 +5,28 @@ int Client::menu() {
 	int code;
 	do {
 		system("cls");
-		cout << "     Меню Пользователя:     " << "\n\n";
+		cout << "     РњРµРЅСЋ РџРѕР»СЊР·РѕРІР°С‚РµР»СЏ:     " << "\n\n";
 		key = (key + 7) % 7;
-		if (key == 0) draw(" ->   Найти договор по его номеру");
-		else  cout << "   Найти договор по его номеру" << endl;
+		if (key == 0) draw(" ->   РќР°Р№С‚Рё РґРѕРіРѕРІРѕСЂ РїРѕ РµРіРѕ РЅРѕРјРµСЂСѓ");
+		else  cout << "   РќР°Р№С‚Рё РґРѕРіРѕРІРѕСЂ РїРѕ РµРіРѕ РЅРѕРјРµСЂСѓ" << endl;
 
-		if (key == 1) draw(" ->   Найти договор по номеру паспорта. ");
-		else  cout << "   Найти договор по номеру паспорта. " << endl;
+		if (key == 1) draw(" ->   РќР°Р№С‚Рё РґРѕРіРѕРІРѕСЂ РїРѕ РЅРѕРјРµСЂСѓ РїР°СЃРїРѕСЂС‚Р°. ");
+		else  cout << "   РќР°Р№С‚Рё РґРѕРіРѕРІРѕСЂ РїРѕ РЅРѕРјРµСЂСѓ РїР°СЃРїРѕСЂС‚Р°. " << endl;
 
-		if (key == 2)  draw(" ->   Найти договор по дате заключения.");
-		else  cout << "   Найти договор по дате заключения." << endl;
+		if (key == 2)  draw(" ->   РќР°Р№С‚Рё РґРѕРіРѕРІРѕСЂ РїРѕ РґР°С‚Рµ Р·Р°РєР»СЋС‡РµРЅРёСЏ.");
+		else  cout << "   РќР°Р№С‚Рё РґРѕРіРѕРІРѕСЂ РїРѕ РґР°С‚Рµ Р·Р°РєР»СЋС‡РµРЅРёСЏ." << endl;
 
-		if (key == 3)  draw(" ->   Посмотреть информацию о себе.");
-		else  cout << "   Посмотреть информацию о себе." << endl;
+		if (key == 3)  draw(" ->   РџРѕСЃРјРѕС‚СЂРµС‚СЊ РёРЅС„РѕСЂРјР°С†РёСЋ Рѕ СЃРµР±Рµ.");
+		else  cout << "   РџРѕСЃРјРѕС‚СЂРµС‚СЊ РёРЅС„РѕСЂРјР°С†РёСЋ Рѕ СЃРµР±Рµ." << endl;
 
-		if (key == 4)  draw(" ->   Изменить логин или пароль.");
-		else  cout << "   Изменить логин или пароль." << endl;
+		if (key == 4)  draw(" ->   РР·РјРµРЅРёС‚СЊ Р»РѕРіРёРЅ РёР»Рё РїР°СЂРѕР»СЊ.");
+		else  cout << "   РР·РјРµРЅРёС‚СЊ Р»РѕРіРёРЅ РёР»Рё РїР°СЂРѕР»СЊ." << endl;
 
-		if (key == 5)  draw(" ->   Изменить информацию о себе.");
-		else  cout << "   Изменить информацию о себе." << endl;
+		if (key == 5)  draw(" ->   РР·РјРµРЅРёС‚СЊ РёРЅС„РѕСЂРјР°С†РёСЋ Рѕ СЃРµР±Рµ.");
+		else  cout << "   РР·РјРµРЅРёС‚СЊ РёРЅС„РѕСЂРјР°С†РёСЋ Рѕ СЃРµР±Рµ." << endl;
 
-		if (key == 6)  draw(" ->   Выйти в главное меню.");
-		else  cout << "   Выйти в главное меню." << endl;
+		if (key == 6)  draw(" ->   Р’С‹Р№С‚Рё РІ РіР»Р°РІРЅРѕРµ РјРµРЅСЋ.");
+		else  cout << "   Р’С‹Р№С‚Рё РІ РіР»Р°РІРЅРѕРµ РјРµРЅСЋ." << endl;
 
 		code = getch();
 		if (code == 80 || code == 115) key++;
@@ -75,11 +75,11 @@ void Client::changelogpass(int type) {
 	int a = users.size();
 	users.erase(std::remove_if(users.begin(), users.end(), [=](Human& a) { return a.login == login; }), users.end());
 	int k = users.size();
-	if (a == k) cout << "Вы ввели некорректные данные. Пользователя с таким логином не существует." << endl;
+	if (a == k) cout << "Р’С‹ РІРІРµР»Рё РЅРµРєРѕСЂСЂРµРєС‚РЅС‹Рµ РґР°РЅРЅС‹Рµ. РџРѕР»СЊР·РѕРІР°С‚РµР»СЏ СЃ С‚Р°РєРёРј Р»РѕРіРёРЅРѕРј РЅРµ СЃСѓС‰РµСЃС‚РІСѓРµС‚." << endl;
 	if (a != k) {
 		while (true) {
 			system("cls");
-			cout << " Введите:  \n 1.Если хотите изменить пароль.\n 2.Если хотите изменить логин и пароль. " << endl;
+			cout << " Р’РІРµРґРёС‚Рµ:  \n 1.Р•СЃР»Рё С…РѕС‚РёС‚Рµ РёР·РјРµРЅРёС‚СЊ РїР°СЂРѕР»СЊ.\n 2.Р•СЃР»Рё С…РѕС‚РёС‚Рµ РёР·РјРµРЅРёС‚СЊ Р»РѕРіРёРЅ Рё РїР°СЂРѕР»СЊ. " << endl;
 			getline(cin, choice);
 			if (choice != "1" && choice != "2") continue;
 			else break;
@@ -105,21 +105,21 @@ void Client::changeinfo(int type) {
 	system("cls");
 	vector<Client> userss;
 	data_client_file(userss);
-	string passport_code = BOO::client_code("Введите номер паспорта для обновления информации.");
+	string passport_code = BOO::client_code("Р’РІРµРґРёС‚Рµ РЅРѕРјРµСЂ РїР°СЃРїРѕСЂС‚Р° РґР»СЏ РѕР±РЅРѕРІР»РµРЅРёСЏ РёРЅС„РѕСЂРјР°С†РёРё.");
 	int a = userss.size();
 	userss.erase(remove_if(userss.begin(), userss.end(), [=](Client& s) { return s.client_code == passport_code; }), userss.end());
 	int k = userss.size();
 	if (k != a) {
 		Client temp;
 		temp.client_code = passport_code;
-		temp.name = ccsns("Введите имя.");
-		temp.surname = ccsns("Введите фамилию.");
-		temp.telephone_number = hfst("Введите номер телефона.",3);
-		temp.country = ccsns("Введите страну проживания.");
-		temp.city = ccsns("Введите город проживания.");
-		temp.street = ccsns("Введите улицу проживания.");
-		temp.housenumber = hfst("Введите номер дома.",1);
-		temp.flatnumber = hfst("Введите номер квартиры, введите 0 ,если клиент проживает в частном доме.",1);
+		temp.name = ccsns("Р’РІРµРґРёС‚Рµ РёРјСЏ.");
+		temp.surname = ccsns("Р’РІРµРґРёС‚Рµ С„Р°РјРёР»РёСЋ.");
+		temp.telephone_number = hfst("Р’РІРµРґРёС‚Рµ РЅРѕРјРµСЂ С‚РµР»РµС„РѕРЅР°.",3);
+		temp.country = ccsns("Р’РІРµРґРёС‚Рµ СЃС‚СЂР°РЅСѓ РїСЂРѕР¶РёРІР°РЅРёСЏ.");
+		temp.city = ccsns("Р’РІРµРґРёС‚Рµ РіРѕСЂРѕРґ РїСЂРѕР¶РёРІР°РЅРёСЏ.");
+		temp.street = ccsns("Р’РІРµРґРёС‚Рµ СѓР»РёС†Сѓ РїСЂРѕР¶РёРІР°РЅРёСЏ.");
+		temp.housenumber = hfst("Р’РІРµРґРёС‚Рµ РЅРѕРјРµСЂ РґРѕРјР°.",1);
+		temp.flatnumber = hfst("Р’РІРµРґРёС‚Рµ РЅРѕРјРµСЂ РєРІР°СЂС‚РёСЂС‹, РІРІРµРґРёС‚Рµ 0 ,РµСЃР»Рё РєР»РёРµРЅС‚ РїСЂРѕР¶РёРІР°РµС‚ РІ С‡Р°СЃС‚РЅРѕРј РґРѕРјРµ.",1);
 		userss.push_back(temp);
 		rewrite_information_file(userss);
 		vector <Client> passport;
@@ -143,7 +143,7 @@ void Client::changeinfo(int type) {
 		}
 		rewrite_contract_file(magic);
 	}
-	else cout << "Такого номера паспорта в нотариальной базе нет." << endl;
+	else cout << "РўР°РєРѕРіРѕ РЅРѕРјРµСЂР° РїР°СЃРїРѕСЂС‚Р° РІ РЅРѕС‚Р°СЂРёР°Р»СЊРЅРѕР№ Р±Р°Р·Рµ РЅРµС‚." << endl;
 	system("pause");
 	if (type == 1) BOO::choice(1);
 	else BOO::choice(2);
@@ -158,13 +158,13 @@ void Client::find_contract_number(int type) {
 	vector<Outputs<string>> dogovors;
 	data_dogovor_file(dogovors);
 	TablePrinter tp(&cout);
-	tp.AddColumn("Номер договора", 14);
-	tp.AddColumn("Код услуги", 10);
-	tp.AddColumn("Название договора", 38);
-	tp.AddColumn("Сумма", 9);
-	tp.AddColumn("Комиссия", 9);
-	tp.AddColumn("Дата подписания", 15);
-	tp.AddColumn("Номер паспорта", 14);
+	tp.AddColumn("РќРѕРјРµСЂ РґРѕРіРѕРІРѕСЂР°", 14);
+	tp.AddColumn("РљРѕРґ СѓСЃР»СѓРіРё", 10);
+	tp.AddColumn("РќР°Р·РІР°РЅРёРµ РґРѕРіРѕРІРѕСЂР°", 38);
+	tp.AddColumn("РЎСѓРјРјР°", 9);
+	tp.AddColumn("РљРѕРјРёСЃСЃРёСЏ", 9);
+	tp.AddColumn("Р”Р°С‚Р° РїРѕРґРїРёСЃР°РЅРёСЏ", 15);
+	tp.AddColumn("РќРѕРјРµСЂ РїР°СЃРїРѕСЂС‚Р°", 14);
 	tp.PrintHeader();
 	for (auto i : dogovors) {
 		if (dogovor_number == i.document_code) {
@@ -176,7 +176,7 @@ void Client::find_contract_number(int type) {
 	if (u == 0) {
 		tp << " " << bprinter::endl();
 		tp.PrintFooter();
-		cout << "Такого номера договора в базе не существует." << endl;
+		cout << "РўР°РєРѕРіРѕ РЅРѕРјРµСЂР° РґРѕРіРѕРІРѕСЂР° РІ Р±Р°Р·Рµ РЅРµ СЃСѓС‰РµСЃС‚РІСѓРµС‚." << endl;
 	}
 	if (type == 1) BOO::choice(1);
 	else BOO::choice(2);
@@ -186,17 +186,17 @@ void Client::find_passport(int type) {
 	system("cls");
 	int u = 0;
 	string passport_code;
-	passport_code = BOO::client_code("Введите номер паспорта.");
+	passport_code = BOO::client_code("Р’РІРµРґРёС‚Рµ РЅРѕРјРµСЂ РїР°СЃРїРѕСЂС‚Р°.");
 	std::vector<Outputs<string>> dogovors;
 	data_dogovor_file(dogovors);
 	TablePrinter tp(&cout);
-	tp.AddColumn("Номер паспорта", 14);
-	tp.AddColumn("Номер договора", 14);
-	tp.AddColumn("Код услуги", 10);
-	tp.AddColumn("Название договора", 38);
-	tp.AddColumn("Сумма", 9);
-	tp.AddColumn("Комиссия", 9);
-	tp.AddColumn("Дата подписания", 15);
+	tp.AddColumn("РќРѕРјРµСЂ РїР°СЃРїРѕСЂС‚Р°", 14);
+	tp.AddColumn("РќРѕРјРµСЂ РґРѕРіРѕРІРѕСЂР°", 14);
+	tp.AddColumn("РљРѕРґ СѓСЃР»СѓРіРё", 10);
+	tp.AddColumn("РќР°Р·РІР°РЅРёРµ РґРѕРіРѕРІРѕСЂР°", 38);
+	tp.AddColumn("РЎСѓРјРјР°", 9);
+	tp.AddColumn("РљРѕРјРёСЃСЃРёСЏ", 9);
+	tp.AddColumn("Р”Р°С‚Р° РїРѕРґРїРёСЃР°РЅРёСЏ", 15);
 	tp.PrintHeader();
 	for (auto i : dogovors) {
 		if (passport_code == i.client_code) {
@@ -208,7 +208,7 @@ void Client::find_passport(int type) {
 	if (u == 0) {
 		tp << " " << bprinter::endl();
 		tp.PrintFooter();
-		cout << "На этот номер паспорта не зарегистрирован договор." << endl;
+		cout << "РќР° СЌС‚РѕС‚ РЅРѕРјРµСЂ РїР°СЃРїРѕСЂС‚Р° РЅРµ Р·Р°СЂРµРіРёСЃС‚СЂРёСЂРѕРІР°РЅ РґРѕРіРѕРІРѕСЂ." << endl;
 	}
 	if (type == 1) BOO::choice(1);
 	else BOO::choice(2);
@@ -219,8 +219,8 @@ void Client::find_date(int type) {
 	int u = 0;
 	bool isLeap = true;
 	int days, months, years;
-	if (type == 2) this->client_code = BOO::client_code("Введите свой номер паспорта.");
-	years = year("Введите год:");
+	if (type == 2) this->client_code = BOO::client_code("Р’РІРµРґРёС‚Рµ СЃРІРѕР№ РЅРѕРјРµСЂ РїР°СЃРїРѕСЂС‚Р°.");
+	years = year("Р’РІРµРґРёС‚Рµ РіРѕРґ:");
 	if (years % 4 == 0) {
 		system("cls");
 		isLeap = true;
@@ -229,19 +229,19 @@ void Client::find_date(int type) {
 		if (years % 400 == 0) isLeap = true;
 		else isLeap = false;
 	}
-	months = month("Введиет месяц:");
-	days = day("Введите день:", isLeap, months);
+	months = month("Р’РІРµРґРёРµС‚ РјРµСЃСЏС†:");
+	days = day("Р’РІРµРґРёС‚Рµ РґРµРЅСЊ:", isLeap, months);
 	string date = to_string_date(days, months, years);
 	vector<Outputs<string>> dogovors;
 	data_dogovor_file(dogovors);
 	TablePrinter tp(&cout);
-	tp.AddColumn("Дата подписания", 15);
-	tp.AddColumn("Номер паспорта", 14);
-	tp.AddColumn("Номер договора", 14);
-	tp.AddColumn("Код услуги", 10);
-	tp.AddColumn("Название договора", 38);
-	tp.AddColumn("Сумма", 9);
-	tp.AddColumn("Комиссия", 9);
+	tp.AddColumn("Р”Р°С‚Р° РїРѕРґРїРёСЃР°РЅРёСЏ", 15);
+	tp.AddColumn("РќРѕРјРµСЂ РїР°СЃРїРѕСЂС‚Р°", 14);
+	tp.AddColumn("РќРѕРјРµСЂ РґРѕРіРѕРІРѕСЂР°", 14);
+	tp.AddColumn("РљРѕРґ СѓСЃР»СѓРіРё", 10);
+	tp.AddColumn("РќР°Р·РІР°РЅРёРµ РґРѕРіРѕРІРѕСЂР°", 38);
+	tp.AddColumn("РЎСѓРјРјР°", 9);
+	tp.AddColumn("РљРѕРјРёСЃСЃРёСЏ", 9);
 	tp.PrintHeader();
 	if (type == 2) {
 		for (auto i : dogovors) {
@@ -264,7 +264,7 @@ void Client::find_date(int type) {
 	if (u == 0) {
 		tp << " " << bprinter::endl();
 		tp.PrintFooter();
-		cout << "В такой день договор не заключали." << endl;
+		cout << "Р’ С‚Р°РєРѕР№ РґРµРЅСЊ РґРѕРіРѕРІРѕСЂ РЅРµ Р·Р°РєР»СЋС‡Р°Р»Рё." << endl;
 	}
 	if (type == 1) BOO::choice(1);
 	else BOO::choice(2);
@@ -274,17 +274,17 @@ void Client::find_user(int type) {
 	system("cls");
 	int u = 0;
 	string passport;
-	passport = BOO::client_code("Введите номер паспорта.");
+	passport = BOO::client_code("Р’РІРµРґРёС‚Рµ РЅРѕРјРµСЂ РїР°СЃРїРѕСЂС‚Р°.");
 	std::vector<Client> info;
 	data_client_file(info);
 	TablePrinter tp(&std::cout);
-	tp.AddColumn("Номер паспорта", 14);
-	tp.AddColumn("Имя и Фамилия", 15);
-	tp.AddColumn("Номер телефона", 14);
-	tp.AddColumn("Страна и Город", 20);
-	tp.AddColumn("Улица", 20);
-	tp.AddColumn("Номер дома", 10);
-	tp.AddColumn("Номер квартиры", 14);
+	tp.AddColumn("РќРѕРјРµСЂ РїР°СЃРїРѕСЂС‚Р°", 14);
+	tp.AddColumn("РРјСЏ Рё Р¤Р°РјРёР»РёСЏ", 15);
+	tp.AddColumn("РќРѕРјРµСЂ С‚РµР»РµС„РѕРЅР°", 14);
+	tp.AddColumn("РЎС‚СЂР°РЅР° Рё Р“РѕСЂРѕРґ", 20);
+	tp.AddColumn("РЈР»РёС†Р°", 20);
+	tp.AddColumn("РќРѕРјРµСЂ РґРѕРјР°", 10);
+	tp.AddColumn("РќРѕРјРµСЂ РєРІР°СЂС‚РёСЂС‹", 14);
 	tp.PrintHeader();
 	for (auto i : info) {
 		if (passport == i.client_code) {
@@ -298,7 +298,7 @@ void Client::find_user(int type) {
 	if (u == 0) {
 		tp << " " << bprinter::endl();
 		tp.PrintFooter();
-		std::cout << "Этого номера паспорта нет в базе." << std::endl;
+		std::cout << "Р­С‚РѕРіРѕ РЅРѕРјРµСЂР° РїР°СЃРїРѕСЂС‚Р° РЅРµС‚ РІ Р±Р°Р·Рµ." << std::endl;
 	}
 	if (type == 1) BOO::choice(1);
 	else BOO::choice(2);
