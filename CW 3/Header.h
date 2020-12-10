3ed0d0b5053a60c351707550fb8af6d53b076e5e
Annette <oiveshka@gmail.com>
2020-12-10 18:11:14 +0300

    Update Header.h

--- CW 3/Header.h
+++ CW 3/Header.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "Sha256.h"
+#include "SHA256.h"
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -25,33 +25,31 @@ class Human;
 class Client;
 class Admin;
 class Contract;
+template<typename T, typename R>
 class Human_actions;
 
 
- 
- 
 int menu_1(int);//меню первого уровня
-string pass(string );//пароль звездочками
+string pass(string);//пароль звездочками
 string yes_no(string);
 void draw(string);//функция для покраски текста
-//string getString(string);//вывод строки
 string client_code(string);//проверка на ввод номера паспорта
 string ccsns(string);//проверка на ввод страны,города, имени , фамилии и тд
-string hfst(string,int);//проверка на ввод номера дома и квартиры
-string contract_name(string );//название договора
+string hfst(string, int);//проверка на ввод номера дома и квартиры
+string contract_name(string);//название договора
 string service_code(string);//код услуги
 
 int get_int(string);//
 int year(string);//проверка на ввод года
 int month(string);//проверк на воод месяца
-int day(string , bool , int&);//провека на ввод дня
-bool current_time(int &, int &, int &, bool );//сравние времени с настоящим
-string to_string_date(int& , int& , int& );// преобразование даты в строку
+int day(string, bool, int&);//провека на ввод дня
+bool current_time(int&, int&, int&, bool);//сравние времени с настоящим
+string to_string_date(int&, int&, int&);// преобразование даты в строку
 
 
-namespace BOO {
+namespace BOO { // пользовательское пронстранство имен 
 	string login(int, bool&);//ввод логина
-	string password(int,bool&);//ввод пароля
+	string password(int, bool&);//ввод пароля
 	void menu(int);//меню
 	string client_code(string);//проверка на номер паспорта
 	string contract_code(int, bool&);//проверка на номер договора
@@ -64,54 +62,54 @@ namespace BOO {
 
 
 
- class Human {
+class Human {
 protected:
 	string login; //логин
 	string password; //пароль
 public:
-	Human(string, string);
-	Human() {};
-	bool operator==(Human human);
+	Human(string, string);//контсруктор с 2 полями типа string
+	Human() = default;//конструктор по умолчанию
+	bool operator==(Human human);// перегрузка оператора ==
 	friend void data_user_file(vector<Human>&);// заполение вектора логинами и паролями пользователей
 	friend void data_admin_file(vector<Human>&);// заполение вектора логинами и паролями администратора
-	static void rewrite_user_file(vector<Human>& );
-    bool check_login_once();//проверить наличие логина на единство
-	~Human() {};
-	friend class Client;
-	friend class Admin;
+	static void rewrite_user_file(vector<Human>&);
+	bool check_login_once();//проверить наличие логина на единство
+	~Human() = default;//деструктор
+	friend class Client;//дружетсвенный класс Client
+	friend class Admin;//дружественный класс Admin
 };
 
- 
- class Human_actions {
- public:
-	 virtual int menu() = 0;//меню
-	 virtual void enterAccount(vector<Human>&) = 0;//вход в аккаунт
- };
+template<typename T, typename R>//шаблонный интерфейс
+class Human_actions {
+public:
+	virtual R menu() = 0;// виртуальная функция меню
+	virtual void enterAccount(vector<T>&) = 0;//виртуальная функция входа в аккаунт
+};
 
- class Client :public Human, virtual public Human_actions {
+class Client :public Human, virtual public Human_actions<Human, int> {//класс Клиент, который наследуется от 2 классов
 protected:
 	string client_code;// код клиента/номер паспорта
-	string name;//Имя
+    string name;//Имя
 	string surname;//Фамилия
-	string telephone_number;//Номер телефона
+	int telephone_number = 0;//Номер телефона
 	string country;//Страна
-    string city;//Город
+	string city;//Город
 	string street;//Улица
-	string housenumber;//Номер дома
-	string flatnumber;//Номер квартиры
-	string quantity;//количество заключенных договоров
- public:
-	Client() {};
-	~Client() {};
-	friend class Admin;
-	int menu() override; //меню пользователя
-	void enterAccount(vector<Human>&) override;// вход в аккаунт пользователя
-	friend void data_passport_file(vector<Client>&);//заполнения вектора с номерами паспортов пользователей
-	friend void data_client_file(vector<Client>&);//заполнение вектора инфы с инфой пользователей
-	friend void rewrite_information_file(vector <Client>&);//перезапись файла с инфой о пользователе
-	friend void rewrite_passport_file(vector <Client>&);//перезапись файла с паспортными данными
-	friend void rewrite_contract_file(vector <Client>&);// перезапись файла с заключенными договорами(номер + имя и фамилия)
-	friend void data_contract_file(vector<Client>& );//заполнение вектора с заключенными договорами(номер + имя и фамилия)
+	int housenumber = 0 ;//Номер дома
+	int flatnumber = 0 ;//Номер квартиры
+	int quantity = 0;//количество заключенных договоров
+public:
+	Client() = default;//конструктор по умолчанию
+	~Client() = default;//деструктор
+	friend class Admin;// дружественный класс Admin
+	int menu() override; //меню пользователя (переопределение)
+	void enterAccount(vector<Human>&) override;//  вход в аккаунт пользователя (переопределение функции)
+	friend void data_passport_file(vector<Client>&);//заполнения вектора с номерами паспортов пользователей (дружественная)
+	friend void data_client_file(vector<Client>&);//заполнение вектора инфы с инфой пользователей (дружественная)
+	friend void rewrite_information_file(vector <Client>&);//перезапись файла с инфой о пользователе (дружественная)
+	friend void rewrite_passport_file(vector <Client>&);//перезапись файла с паспортными данными (дружественная)
+	friend void rewrite_contract_file(vector <Client>&);// перезапись файла с заключенными договорами(номер + имя и фамилия) (дружествнная)
+	friend void data_contract_file(vector<Client>&);//заполнение вектора с заключенными договорами(номер + имя и фамилия) (дружественная)
 	void changelogpass(int);//сменить логин/пароль
 	void changeinfo(int);//смена информации о пользователе
 	void find_contract_number(int);//поиск договор по его номеру
@@ -120,10 +118,10 @@ protected:
 	void find_user(int);//поиск информации о пользователе
 };
 
- class Admin :public Client {
+class Admin :public Client {
 public:
-	void enterAccount(vector<Human>&) override;//вход в аккаунт администратора
-	int menu() override; //меню администратора
+	void enterAccount(vector<Human>&) override;//вход в аккаунт администратора (переопределение функции)
+	int menu() override; //меню администратора (переопределение)
 	void conclusion_contract();// заключение договора
 	bool add_logpass(); // добавить логин и пароль пользователя
 	void add_user_information_contract();//добавление инфы о юзере при заключении договра
@@ -139,46 +137,45 @@ public:
 };
 
 
- class Contract {
- protected:
-	 string contract_code;//Код договора
- public:
-	 Contract() {};
-	 ~Contract() {};
-	 string name;//Название договора
-	 string service_code;// Код услуги
-	 string summ;//Сумма
-	 string commition;//Комисcионные
-	 friend void contract_code(vector<Contract>&);//заполнение вектора с номерами договоров
-	 friend class Admin;
-	 friend class Client;
-	 void add_document_code();//добавить код договора
+class Contract {
+protected:
+	string contract_code;//Код договора
+public:
+	Contract() = default; // конструктор по умолчанию
+	~Contract() = default;//деструктор
+	string name;//Название договора
+	string service_code;// Код услуги
+	int summ = 0;//Сумма
+	int commition = 0;//Комисcионные
+	friend void contract_code(vector<Contract>&);//заполнение вектора с номерами договоров
+	friend class Admin;// дружественный класс Admin
+	friend class Client;//дружественный класс Client
+	void add_document_code();//добавить код договора
 };
 
- template <class T>
- class Outputs {
- protected:
-	 string client_code;//номер паспорта
-	 string service_code;//код услуги
-	 string name;//название договора
-	 string summ;//сумма
-	 string comission;//комиссионные
-	 string document_code;//номер договора
-	 string date;//дата подписания
- public:
-	 Outputs() {};
-	 ~Outputs() {};
-	 friend void data_dogovor_file(vector<Outputs>&);//заполнение вектора с заключенными договорами
-	 friend class Admin;
-	 friend class Client;
- };
+class Outputs {
+protected:
+	string client_code;//номер паспорта
+	string service_code;//код услуги
+	string name;//название договора
+	int summ = 0;//сумма
+	int comission = 0;//комиссионные
+	string document_code;//номер договора
+	string date;//дата подписания
+public:
+	Outputs() = default;// конструктор по умелчанию
+	~Outputs() = default;//деструктор
+	friend void data_dogovor_file(vector<Outputs>&);//заполнение вектора с заключенными договорами
+	friend class Admin;// дружественный класс Admin
+	friend class Client;//дружественный класс Client
+};
 
- template <typename T>
- string get_string(T p)
- {
-	 system("cls");
-	 string mystring;
-	 cout << p << endl;
-	 getline(cin, mystring);
-	 return mystring;
- }
+template <typename T> // шаблонная функция получения строки
+string get_string(const T& p)
+{
+	system("cls");
+	string mystring;
+	cout << p << endl;
+	getline(cin, mystring);
+	return mystring;
+}
